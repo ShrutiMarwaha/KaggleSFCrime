0e0a7ee0bb295db122ca59ca75115d7c95c39f17
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-05-14 20:51:44 -0700

    function to convert longitude and latitude into zip code

--- processors/feature_engineering.py
+++ processors/feature_engineering.py
@@ -2,3 +2,5 @@ from pyzipcode import ZipCodeDatabase
 
 zcdb = ZipCodeDatabase()
 sf_zipcodes = zcdb.find_zip(city="San Francisco", state="CA")
+
+# function to convert longitude and latitude into zip code
