4a563ba0c85b5b7ef5f46069131488983ad7ed66
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-05-26 12:59:51 -0700

    added a function to convert longitude and latitude to zipcode

--- processors/feature_engineering.py
+++ processors/feature_engineering.py
@@ -1,35 +1,39 @@
 from pyzipcode import ZipCodeDatabase
 from scipy.spatial import distance
-import pandas as pd
 
 zcdb = ZipCodeDatabase()
 sf_zipcodes = zcdb.find_zip(city="San Francisco", state="CA")
 print(len(sf_zipcodes))
 
-# extract longitude, latitude and zip codes from sf_zipcodes
-sf_zipcodes_df = pd.DataFrame()
+def closest_zipcode(input_longitude,input_latitude):
+    """
+    function to convert latitude and longitude to zipcode:
+    find euclidean distance between user provided latitude and longitude and
+    all latitudes, longitudes from sf_zipcodes and select the row from latter
+    which has minimum distance. then extract its zip code
+    :param input_longitude,input_latitude: latitude and longitude that you want to convert to closest zipcode
+    :return closest_zip: zip code
+    """
 
-for sf_zipcode in sf_zipcodes:
-    temp_dict = {'latitude':sf_zipcode.latitude, 'longitude':sf_zipcode.longitude, 'zipcode':int(sf_zipcode.zip)}
-    temp_df = pd.DataFrame(temp_dict, index=[0])
-    sf_zipcodes_df = pd.concat([sf_zipcodes_df, temp_df])
+    closest_zip = None
+    euc_dist = None
 
-print(sf_zipcodes_df.shape)
-print(sf_zipcodes_df.head())
+    for i, sf_zipcode in enumerate(sf_zipcodes):
+        # extract latitude and longitude from each row in sf_zipcode
+        lat = sf_zipcode.latitude
+        long = sf_zipcode.longitude
+        # calculate euclidean distance between lat and long from sf_zipcode and input latitude and longitude value
+        euclidean_dist = round( distance.euclidean((long,lat), (input_longitude,input_latitude)), 4)
+        # assign the euclidean distance calculated for first row as euc_dist
+        if i == 0:
+            euc_dist = euclidean_dist
 
-X=-122.425
-Y=37.774
+        # if the euclidean_dist is smaller than the one calculated in previous iteration,
+        # replace it with current one and extract the corresponding zipcode
+        if euclidean_dist < euc_dist:
+            euc_dist = euclidean_dist
+            closest_zip = int(sf_zipcode.zip)
 
-for sf_zipcode in sf_zipcodes:
-    lat = sf_zipcode.latitude
-    long = sf_zipcode.longitude
-    euclidean_dist = round( distance.euclidean((long,lat) , (X,Y)), 4)
-    print(euclidean_dist)
-    #print(sf_zipcode.zip)
+    return closest_zip
 
-
-
-
-
-zips = [sf_zipcode.zip for sf_zipcode in sf_zipcodes]
-# function to convert longitude and latitude into zip code
+# closest_zipcode(-122.4033, 37.71343)
