fcbf7681223334610b87a50f2382b2f0d60d214b
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-05-26 13:41:17 -0700

    added code to call feature engineering function

--- bin/sfcrime.py
+++ bin/sfcrime.py
@@ -1,8 +1,10 @@
 import pandas as pd
-
 # local functions/processors
-from processors import loader, feature_extractor as extractor
+from processors import loader
+from processors import feature_extractor as extractor
+from processors import feature_engineering as engineering
 
+# load data
 training_set = loader.load_csv_data("/Users/shruti/Desktop/WorkMiscellaneous/MachineLearning/SanFranciscoCrime/train.csv")
 test_set = loader.load_csv_data("/Users/shruti/Desktop/WorkMiscellaneous/MachineLearning/SanFranciscoCrime/test.csv")
 
@@ -17,12 +19,28 @@ print "first 5 rows of column Category of training set: \n %s \n" % training_set
 print "dimension of test data: %s \n" % (test_set.shape,)
 print "test data: \n %s \n" % test_set.head()
 
+# feature extraction
 training_striped_time = extractor.extract_date_dataframe(training_set.Dates)
 test_striped_time = extractor.extract_date_dataframe(test_set.Dates)
 
-training_features = pd.concat( (training_striped_time,training_set[["DayOfWeek","PdDistrict","X","Y","Category"]]), axis=1)
+# feature engineering
+training_zipcodes = training_set.apply(lambda d: engineering.closest_zipcode(d["X"],d["Y"]), axis=1)
+training_zipcodes.name = "zip"
+len(training_zipcodes.unique())
+
+test_zipcodes = test_set.apply(lambda d: engineering.closest_zipcode(d["X"],d["Y"]), axis=1)
+test_zipcodes.name = "zip"
+#training_zipcodes.to_pickle("/Users/shruti/Desktop/WorkMiscellaneous/MachineLearning/SanFranciscoCrime/training_zipcodes.pkl")
+
+# combine important features
+training_features = pd.concat( (training_striped_time,training_zipcodes,training_set[["DayOfWeek","PdDistrict","Category"]]), axis=1)
 print "after features extraction - training data: \n %s \n" % training_features.head()
-test_features = pd.concat( (test_striped_time,test_set[["DayOfWeek","PdDistrict","X","Y"]]), axis=1)
+test_features = pd.concat( (test_striped_time,test_zipcodes,test_set[["DayOfWeek","PdDistrict"]]), axis=1)
 print "after features extraction - training datatest data: \n %s \n" % test_features.head()
 
+# training_features.to_pickle("/Users/shruti/Desktop/WorkMiscellaneous/MachineLearning/SanFranciscoCrime/training_features.pkl")
+# test_features.to_pickle("/Users/shruti/Desktop/WorkMiscellaneous/MachineLearning/SanFranciscoCrime/test_features.pkl")
+
+# load training set features
+#training_features = pd.read_pickle("/Users/shruti/Desktop/WorkMiscellaneous/MachineLearning/SanFranciscoCrime/training_features.pkl")
 
