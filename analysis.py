0572e8ac9bbd40968c262114c65fd58e83be2403
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-05-27 15:48:44 -0700

    data_summary function tells basics about the dataset

---
+++ analysis.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def data_summary(df):
+    '''
+    print basic head, dimension, columnames
+    :param df: data frame
+    :return: dimension, missing data, column names, top 3 lines
+    '''
+    print "dataset dimensions: %s \n" % (df.shape,)
+    print "number of rows without missing data: %s \n" % np.count_nonzero(df.isnull())
+    print "columns of dataset: %s \n" % df.columns.tolist()
+    print "first 3 rows: \n%s" % df.iloc[:3,:]
+
+
+
