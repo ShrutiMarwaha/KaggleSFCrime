76f7b5c019622cc264d0db81a9e724feaec66f81
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-05-27 15:23:28 -0700

    renamed user input data_frame to df

--- processors/feature_extractor.py
+++ processors/feature_extractor.py
@@ -2,11 +2,11 @@ from datetime import datetime
 import pandas as pd
 
 # function to extract year, month, date and hour from one column in dataframe
-def extract_date_dataframe(data_frame):
-    year_dataframe = data_frame.apply(lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S").year)
-    month_dataframe = data_frame.apply(lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S").month)
-    date_dataframe = data_frame.apply(lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S").day)
-    hour_dataframe = data_frame.apply(lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S").hour)
+def extract_date_dataframe(df):
+    year_dataframe = df.apply(lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S").year)
+    month_dataframe = df.apply(lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S").month)
+    date_dataframe = df.apply(lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S").day)
+    hour_dataframe = df.apply(lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S").hour)
 
     new_dataframe = pd.concat([year_dataframe,month_dataframe,date_dataframe,hour_dataframe], axis=1)
     new_dataframe.columns = ["year","month","date","hour"]
