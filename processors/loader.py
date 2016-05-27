7ebf6904522869410fdb8bbe168a15b750b3d1ff
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-05-27 11:17:00 -0700

    added argument nrows so that while testing user can import only few rows

--- processors/loader.py
+++ processors/loader.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-def load_csv_data(filename):
+def load_csv_data(filename,nrows=None):
     """
     reads csv file and returns data-frame using pandas
     :param filename: csv file
     :return: data-frame (pandas)
     """
 
-    output = pd.read_csv(filename)
+    output = pd.read_csv(filename,nrows=nrows)
     return(output)
