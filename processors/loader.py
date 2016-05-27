9790e1bb40797615ac5eba286b615f164843f478
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-05-27 14:04:19 -0700

    renamed nrows as n_rows

--- processors/loader.py
+++ processors/loader.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-def load_csv_data(filename,nrows=None):
+def load_csv_data(filename,n_rows=None):
     """
     reads csv file and returns data-frame using pandas
     :param filename: csv file
     :return: data-frame (pandas)
     """
 
-    output = pd.read_csv(filename,nrows=nrows)
+    output = pd.read_csv(filename,nrows=n_rows)
     return(output)
