40b905504a1178ea87886436e9f90cfe41fb8b24
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-05-27 17:22:42 -0700

    concatenate new columns to the original data frame

--- processors/feature_extractor.py
+++ processors/feature_extractor.py
@@ -1,16 +1,18 @@
 from datetime import datetime
 import pandas as pd
 
-def extract_date_dataframe(df):
+def extract_date_dataframe(df,colname):
     '''
     function to extract year, monthand hour from one column in data frame
     :param df: data frame
-    :return new_dataframe: data frame with additional columns - year, month and hour
+    :return new_dataframe: original data frame with additional columns - year, month and hour
     '''
-    year_dataframe = df.apply(lambda d: pd.Series({'year': datetime.strptime(d, "%Y-%m-%d %H:%M:%S").year}))
-    month_dataframe = df.apply(lambda d: pd.Series({'month': datetime.strptime(d, "%Y-%m-%d %H:%M:%S").month}))
-    #date_dataframe = df.apply(lambda d: pd.Series({'date': datetime.strptime(d, "%Y-%m-%d %H:%M:%S").day}))
-    hour_dataframe = df.apply(lambda d: pd.Series({'hour': datetime.strptime(d, "%Y-%m-%d %H:%M:%S").hour}))
+    data_col = df[colname]
+
+    year_dataframe = data_col.apply(lambda d: pd.Series({'year': datetime.strptime(d, "%Y-%m-%d %H:%M:%S").year}))
+    month_dataframe = data_col.apply(lambda d: pd.Series({'month': datetime.strptime(d, "%Y-%m-%d %H:%M:%S").month}))
+    #date_dataframe = data_col.apply(lambda d: pd.Series({'date': datetime.strptime(d, "%Y-%m-%d %H:%M:%S").day}))
+    hour_dataframe = data_col.apply(lambda d: pd.Series({'hour': datetime.strptime(d, "%Y-%m-%d %H:%M:%S").hour}))
 
     new_dataframe = pd.concat([df,year_dataframe,month_dataframe,hour_dataframe], axis=1)
 
