7e2c07a7d7ba881f9b302d5aa124a20409c7e2ab
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-05-28 13:25:32 -0700

    function to build model and print results

--- processors/modeling.py
+++ processors/modeling.py
@@ -1,4 +1,11 @@
 import pandas as pd
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import BernoulliNB
+
 
 def create_dummy_var(df,categorical_col):
     """
@@ -12,3 +19,34 @@ def create_dummy_var(df,categorical_col):
 
     dummy_var_df = pd.get_dummies(df)
     return dummy_var_df
+
+def basic_model(mlalgo,train_variables,train_output,test_variables,test_output):
+    if mlalgo=="BernoulliNB":
+        model = BernoulliNB()
+    elif mlalgo=="SVC":
+        model = SVC()
+    elif mlalgo=="GradientBoostingClassifier":
+        model = GradientBoostingClassifier(random_state=0)
+    elif mlalgo=="RandomForestClassifier":
+        model=RandomForestClassifier(n_jobs=-1,random_state=0)
+    else:
+        model = LogisticRegression(n_jobs=-1,random_state=0)
+
+    # build the model
+    model.fit(train_variables, train_output)
+
+    expected = test_output
+    # make predictions
+    predicted = model.predict(test_variables)
+
+    # summarize the fit of the model
+    print(metrics.classification_report(expected, predicted))
+    print(metrics.confusion_matrix(expected, predicted))
+    # print(metrics.roc_auc_score(expected, predicted)) # predicted outputs have to be binarized
+    return metrics.accuracy_score(expected, predicted)
+
+# def basic_model(mlalgo,train_variables,train_output,test_variables,test_output):
+#     if mlalgo=="BernoulliNB":
+#         model = BernoulliNB()
+#     elif mlalgo=="SVC":
+#         model = SVC()
